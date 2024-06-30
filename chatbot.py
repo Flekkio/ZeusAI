d075c925b21d0db7d17fccdc90730d41b710e41d
ALTABER <thibault.altaber@cgi.com>
2024-07-01 00:52:22 +0200

    first version

---
+++ chatbot.py
@@ -0,0 +1,54 @@
+import gradio as gr
+import ollama
+
+css_path = "styles.css"
+
+def format_history(msg: str, history: list[list[str, str]], system_prompt: str):
+    chat_history = [{"role": "system", "content": system_prompt}]
+    for query, response in history:
+        chat_history.append({"role": "user", "content": query})
+        chat_history.append({"role": "assistant", "content": response})  
+    chat_history.append({"role": "user", "content": msg})
+    return chat_history
+
+def generate_response(msg: str, history: list[list[str, str]], system_prompt: str):
+    chat_history = format_history(msg, history, system_prompt)
+    response = ollama.chat(model='llama2', stream=True, messages=chat_history)
+    message = ""
+    for partial_resp in response:
+        token = partial_resp["message"]["content"]
+        message += token
+        yield message
+
+# Exemples correctement formatés
+custom_examples = [
+    ["Qui est Napoléon?"],
+    ["Raconte-moi une blague."],
+    ["Quelle est la capitale de la France?"]
+]
+
+chatbot = gr.ChatInterface(
+    generate_response,
+    chatbot=gr.Chatbot(
+        avatar_images=["user.jpg", "chatbot.png"],
+        height="64vh"
+    ),
+    additional_inputs=[
+        gr.Textbox(
+            "Behave as if you are a professional writer.",
+            label="Ajouter prompt"
+        )
+    ],
+    title="ZEUS AI",
+    textbox=gr.Textbox(placeholder="Posez-moi une question", container=False, scale=7),
+    description="Intelligence artificielle par Thibault Altaber",
+    theme="Taithrah/Minimal",
+    examples=custom_examples,  # Utiliser la liste de listes pour les exemples
+    submit_btn="⬅ Envoyer",
+    retry_btn="🔄 Rafraîchir Réponse",
+    undo_btn="↩ Supprimer",
+    clear_btn="🗑️ Nettoyer Chat",
+    css=css_path
+)
+
+chatbot.launch()
